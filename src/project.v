cea6621b1bbed57d0bdc5ec794469918a9b81474
Xander Gring <xanderg@icloud.com>
2025-02-09 23:11:28 -0600

    Add seconds output

--- src/project.v
+++ src/project.v
@@ -11,25 +11,13 @@
  * The meridiems (AM and PM) are represented using 1 bit (AM=0 PM=1)
  * There are not enough outputs available to include seconds
  *
- * The final design with the LEDs in place should look like this example:
- * ----------------------------------
- * Bit | Hours   Minutes   Meridiem
- * 32  |            1          
- * 16  |            0
- * 8   |  0         0
- * 4   |  1         1
- * 2   |  1         0         
- * 1   |  0         1          1
- * ----------------------------------
- * Dec    6    :    37         PM
- * 
- * Or sideways:              
+ * The final design with the LEDs in place should look like this example:   
  * ----------------------------------
  * Bit      | 32 16 8 4 2 1 | Decimal
  * ----------------------------------
  * Hours    |       0 1 1 0 | 6
  * Minutes  |  1  0 0 1 0 1 | 37
- * Meridiem |             1 | PM
+ * Seconds  |  0  1 0 0 1 0 | 18
  * ----------------------------------
  */
 
@@ -50,22 +38,20 @@ module tt11_um_obliviouX (
     .clk_i(clk),                // internal clock
     .rstn_i(rst_n),             // internal reset
 
-    .hour_id(ui_in[4:3]),       // hour increment/decrement
-    .minute_id(ui_in[2:1]),     // minute increment/decrement
-    .meridiem_id(ui_in[0]),     // meridiem toggle
+    .hour_id(ui_in[5:4]),       // hour increment/decrement
+    .minute_id(ui_in[3:2]),     // minute increment/decrement
+    .seconds_id(ui_in[1:0]),    // seconds increment/decrement
 
-    .hour_out(uo_out[4:0]),     // hour LED output, 5 LEDs
-    .minute_out(uio_out[6:1]),  // minute LED output, 6 LEDs
-    .meridiem_out(uio_out[0])   // meridiem LED output, 1 LED
+    .hour_out(uo_out[7:4]),                    // hour LED output, 4 LEDs
+    .minute_out({uo_out[3:0], uio_out[7:6]}),  // minute LED output, 6 LEDs , this needs to be split between uo_out and uio_out
+    .seconds_out(uio_out[5:0])                 // seconds LED output, 6 LEDs
   );
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uo_out[7:5] = 0;
-  assign uio_out[7] = 0;
-  assign uio_oe  = 8'b00000000;
+  assign uio_oe[7:0]  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0, uio_in, ui_in[7:6]};
 
 endmodule
