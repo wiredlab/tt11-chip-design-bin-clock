8f693e91e98ec9eb6082801a712a27b2258037c7
Xander <155843003+obliviouX@users.noreply.github.com>
2025-02-17 06:31:01 +0000

    Update top module name

--- src/project.v
+++ src/project.v
@@ -23,7 +23,7 @@
 
 `default_nettype none
 
-module tt11_um_obliviouX (
+module tt_um_obliviouX (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
