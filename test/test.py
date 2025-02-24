ee7ff4ab6284c2bd98024774716139ec5aefd542
Xander Gring <xanderg@icloud.com>
2025-02-23 22:36:25 -0600

    check seconds counter

--- test/test.py
+++ test/test.py
@@ -11,17 +11,17 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 10, units="ms")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+#    dut._log.info("Reset")
+#    dut.ena.value = 1
+#    dut.ui_in.value = 0
+#    dut.uio_in.value = 0
+#    dut.rst_n.value = 0
+#    await ClockCycles(dut.clk, 10)
+#    dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
@@ -30,11 +30,11 @@ async def test_project(dut):
     dut.uio_in.value = 0
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 70000)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 183
+    # assert dut.uo_out.value == 183
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
