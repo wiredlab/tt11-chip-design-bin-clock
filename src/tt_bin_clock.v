076b29fe96132f7a92d85cf7ccbfa221480b2910
Xander Gring <xanderg@icloud.com>
2025-02-23 20:08:49 -0600

    add clk and time set logic

--- src/tt_bin_clock.v
+++ src/tt_bin_clock.v
@@ -2,7 +2,7 @@
 
 module tt_bin_clock (
     input wire clk_i,
-    input wire rstn_i,
+    input wire reset_i,
 
     input wire time_set,     // 1 bit - change time or let clock run
     input wire id_switch,    // 1 bit - choose to increment or decrement time
@@ -17,10 +17,49 @@ module tt_bin_clock (
 
 );
 
-// for testing
-assign hour_out = 11;        // 1011
-assign minute_out = 30;      // 0111 10
-assign seconds_out = 45;     // 101101
+    reg[99:0] clk_cnt = 0;  // external clk is 100Hz, so need to count to 100 to output at 1Hz
+    reg[3:0] hours = 0;     // registers to hold time values
+    reg[5:0] minutes = 0;
+    reg[5:0] seconds = 0;
+
+    always @(posedge reset_i) begin   // reset handler
+        if (reset_i) begin
+            clk_cnt <= 0;
+            hours <= 0;
+            minutes <= 0;
+            seconds <= 0;
+        end
+    end
+
+    always @(posedge time_set) begin    // if switch to set time is on
+        if (id_switch == 1) begin       // if increment chosen
+            if (seconds_id == 1) begin
+                seconds <= seconds + 1;
+            end
+            else if (minute_id == 1) begin
+                minutes <= minutes + 1;
+            end
+            else if (hour_id == 1) begin
+                hours <= hours + 1;
+            end
+        else                            // if decrement chosen
+            if (seconds_id == 1) begin
+                seconds <= seconds - 1;
+            end
+            else if (minute_id == 1) begin
+                minutes <= minutes - 1;
+            end
+            else if (hour_id == 1) begin
+                hours <= hours - 1;
+            end
+        end
+    end
+
+
+    // for testing
+    // assign hour_out = 11;        // 1011
+    // assign minute_out = 30;      // 0111 10
+    // assign seconds_out = 45;     // 101101
 
 endmodule
 
